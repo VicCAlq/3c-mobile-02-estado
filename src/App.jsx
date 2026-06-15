3cc530f1175519cc5807aa1221d38a34cd723c7f
elomorais2 <mariaeloiza.morais@nave.org.br>
2026-06-15 09:05:12 -0300

    em processo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Placar from './components/Placar';
 import InputTexto from './components/InputTexto';
+import AtvDobra from './components/AtvDobra';
+import AtvTelefoneSemFio from './components/AtvBotaoAcaoExterna';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <AtvDobra numero={3}/>
+      <AtvTelefoneSemFio></AtvTelefoneSemFio>
       <Placar/>
       <InputTexto/>
       <StatusBar style="auto" />
