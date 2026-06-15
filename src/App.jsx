a92f66b15c6ec323be70238f1281856bd1f9ead8
AliceMarchi <alice.marchi@nave.org.br>
2026-06-15 09:08:47 -0300

    app.jsx importação

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Placar from './components/Placar';
 import InputTexto from './components/InputTexto';
-
+import AtvDobra from './components/AtvDobra';
 export default function App() {
   return (
     <View style={styles.container}>
+      <AtvDobra/>
       <Placar/>
       <InputTexto/>
       <StatusBar style="auto" />
