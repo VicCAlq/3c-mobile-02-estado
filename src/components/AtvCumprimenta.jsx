d9cbcbbae91c516c7446d26f0a1e45af1d339de2
Renan o Ruim <pedro.renan@nave.org.br>
2026-06-15 09:04:56 -0300

    falta completar AtvCumprimenta

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,65 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+import React, {useState} from "react";
+import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
+
+const estiloRoubado = StyleSheet.create({
+     view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+})
+
+export default function AtvCumprimenta() {
+const [valorEscrito, setValorEscrito] = useState("")
+const [textin, setTextin] = useState("É um prazer lhe conhecer") 
+
+
+    return (
+        <View style={estiloRoubado.view}>
+            <Text>
+                Olá, qual seu nome?
+            </Text>
+            <TextInput
+            value={valorEscrito}
+            onChangeText={setValorEscrito}
+            placeholder="Nome Aqui"
+            >
+            </TextInput>
+            <Pressable onPress={() => setTextin(valorEscrito)} style={estiloRoubado.botao}>
+                <Text style={estiloRoubado.textoBotao}>
+                    Salvar
+                </Text>
+            </Pressable>
+            <Text style={estiloRoubado.texto}>{textin}</Text>
+        </View>
+    )
+}
--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,35 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <View> contendo um <Text>, que vai exibir este número recebido como argumento, 
 e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+import React, { useState }  from "react";
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+const estilolegal = StyleSheet.create({
+    botao : {
+        width: "50px",
+        height: "50px",
+        backgroundColor: "blue",
+    },
+    texto: {
+        color: "red",
+        fontSize: "32px"
+    
+    }
+})
+export default function AtvDobra({numero}) {
+    const [Contadorzin, setContar] = useState(numero)
+    function dobrar() {
+        setContar(Contadorzin * 2);
+    }
+    return (
+        <View>
+            <Text style={estilolegal.texto}>
+            {Contadorzin}
+            </Text>
+        <Pressable onPress={() => dobrar()} style={estilolegal.botao}>
+            <Text>
+                CLICA AQUI
+            </Text>
+        </Pressable>
+        </View>
+    )
+}
