2ad0c3be8d5d2bf52a80d755e9c415cd6f2d1b87
tiu10ney <caua.juan@nave.org.br>
2026-06-18 08:48:01 -0300

    dobra feita

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -10,18 +10,20 @@ e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o 
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import { useState } from 'react'
 
- return(
- <View style={estilo.view}>
+export default function AtvDobra({numero}) {
+
+  const [valor, setValor] = useState(numero)
+ 
+  return(
+  <View style={estilo.view}>
       <Text style={estilo.texto}>
-        {placar}
+        {valor}
       </Text>
       <View style={estilo.viewBotoes}>
-        <Pressable style={estilo.botao} onPress={() => marcarPonto()}>
-          <Text style={estilo.textoBotao}>Mais um</Text>
-        </Pressable>
-        <Pressable style={estilo.botao} onPress={() => perderPonto()}>
-          <Text style={estilo.textoBotao}>Menos um</Text>
+        <Pressable style={estilo.botao} onPress={() => setValor(valor * 2)}>
+          <Text style={estilo.textoBotao}>Dobra</Text>
         </Pressable>
       </View>
     </View>
   )
+}
