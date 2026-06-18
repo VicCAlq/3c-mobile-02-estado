20c53d5eec81c51a71a9b9ce44f1756d33e5fbb5
Mike <izabella.cristine@nave.org.br>
2026-06-18 03:11:32 +0000

    Atv 1 pronta

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -7,3 +7,69 @@ Cada <Pressable> ao ser clicado, deve inserir na <View> abaixo um <Text> e um <T
 e substituir o conteúdo anterior dessa <View> de baixo dos botões.
 O texto digitado no <TextInput> ativado por um <Pressable> deve ser exibido no <Text> do outro <Pressable>, e vice-versa.
 */
+
+import { useState } from "react";
+import { Pressable, Text, View, StyleSheet } from "react-native";
+import { TextInput } from "react-native";
+const estilo = StyleSheet.create({
+    botoes:{
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        margin:'10px',
+        padding: '10px',
+        gap:'10px'
+    },
+    botao:{
+        backgroundColor:'white',
+        padding:'10px'
+    },
+    view:{
+         backgroundColor:'gray',
+         alignItems:"center"
+    },
+    output:{
+        color:"white",
+        padding:"10px",
+        gap:"10px"
+    }
+})
+
+export default function AtvTelefoneSemFio({}){
+
+    const [subs, setSubs] = useState(0)
+    const [texto, setTexto]  = useState("")
+    const [texto2, setTexto2] = useState("")
+
+    const paginaRender = () => {
+        if(subs === 1){
+            return < >
+                <Text> {texto2} </Text> 
+                <TextInput 
+                placeholder="Digite uma cor"
+                value={texto}
+                onChangeText={setTexto}/>
+            </>
+        } else if(subs === 2){
+            return <>
+                <Text> {texto} </Text> 
+                <TextInput 
+                placeholder="Digite um nome"
+                value={texto2}
+                onChangeText={setTexto2}/>
+            </>
+        }
+    }
+    
+    return(<>
+        <View style={estilo.view}>
+            <View style={estilo.botoes}>
+                <Pressable style={estilo.botao} onPress={()=> setSubs(1)}>Clique aqui</Pressable>
+                <Pressable style={estilo.botao} onPress={()=> setSubs(2)}>Ou aqui</Pressable>
+            </View>
+            <View style={estilo.output}>
+              {paginaRender()}      
+            </View>
+        </View>
+    </>);
+}
