59a2d8af8dcc39a733ff7d9925266bcfc5ab8f8e
tiu10ney <caua.juan@nave.org.br>
2026-06-18 08:44:27 -0300

    respondido dobra

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,23 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <View> contendo um <Text>, que vai exibir este número recebido como argumento, 
 e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+
+
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { useState } from 'react'
+
+ return(
+ <View style={estilo.view}>
+      <Text style={estilo.texto}>
+        {placar}
+      </Text>
+      <View style={estilo.viewBotoes}>
+        <Pressable style={estilo.botao} onPress={() => marcarPonto()}>
+          <Text style={estilo.textoBotao}>Mais um</Text>
+        </Pressable>
+        <Pressable style={estilo.botao} onPress={() => perderPonto()}>
+          <Text style={estilo.textoBotao}>Menos um</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
