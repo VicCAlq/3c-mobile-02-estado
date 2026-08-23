8b430fde065017184957e7f0454f16493a2ab706
marapessoa06 <samara.pessoa@nave.org.br>
2026-08-23 20:57:15 -0300

    .

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -7,3 +7,106 @@ Cada <Pressable> ao ser clicado, deve inserir na <View> abaixo um <Text> e um <T
 e substituir o conteúdo anterior dessa <View> de baixo dos botões.
 O texto digitado no <TextInput> ativado por um <Pressable> deve ser exibido no <Text> do outro <Pressable>, e vice-versa.
 */
+
+import React, { useState } from "react";
+import { View, Text, Pressable, StyleSheet, TextInput } from "react-native";
+
+const telefoneCss = StyleSheet.create({
+      view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    backgroundColor: "#303540",
+    padding: 10,
+    margin: 10,
+    gap: 10,
+  },
+
+  texto: {
+    color: "#eee",
+     fontSize: 20,
+  },
+
+  textoBotao: {
+    color: "#eee",
+    fontSize: 16,
+  },
+
+  botao: {
+    borderRadius: 5,
+    backgroundColor: "#505560",
+    padding: 10,
+  },
+
+  viewBotoes: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  input: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 8,
+    marginRight: 10,
+  },
+})
+export default function AtvTelefoneSemFio(){
+    const [pagina, setPagina] = useState(0)
+    const [texto1, setTexto1] = useState("")
+    const [texto2, setTexto2] = useState("")
+
+        function primeira(){
+            setPagina(1)
+        }
+          function segunda(){
+            setPagina(-1)
+        }
+
+    return(
+        <View style={telefoneCss.view}>
+            <View style={telefoneCss.viewBotoes}>
+                <Pressable onPress={() => primeira()}  style={telefoneCss.botao}>
+                    <Text> botão 1 </Text>
+                </Pressable>
+                <Pressable onPress={() => segunda()} style={telefoneCss.botao}>
+                    <Text> botão 2 </Text>
+                </Pressable>
+                <View>
+                    
+                </View>
+            </View>
+           
+
+
+
+            {pagina === 1 && (
+               
+            <View>
+                <Text style={telefoneCss.texto}> {texto1} </Text>
+                  <TextInput
+                         style={telefoneCss.input}
+                         value={texto2}
+                         onChangeText={setTexto2}
+                         placeholder="Nome aqui"
+                       />
+            </View>
+             
+            )}
+
+            {pagina === -1 && (
+                <View>
+                <Text style={telefoneCss.texto}> {texto2} </Text>
+                  <TextInput
+                         style={telefoneCss.input}
+                         value={texto1}
+                         onChangeText={setTexto1}
+                         placeholder="Nome aqui"
+                       />
+            </View>
+            )}
+        </View>
+
+    )
+}
--- src/components/AtvMudaConteudo.jsx
+++ src/components/AtvMudaConteudo.jsx
@@ -8,3 +8,73 @@ Ao clicar no segundo <Pressable>, o conteúdo dessa <View> deve ser substituído
 onde cada uma destas <Views> deve ter o tamanho de 200px de altura por 200px de largura, 
 a da esquerda deve ter a cor "purple", a do meio a cor "teal" e a da direita a cor "orange".
 */
+
+import React, { useState } from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+const Estilinhos = StyleSheet.create({
+botoes:{
+    borderRadius: 5,
+    backgroundColor: "#505560",
+    padding: 10,
+},
+direita:{
+    width: 200,
+    height: 200,
+    backgroundColor: "purple" 
+},
+meio:{
+    width: 200,
+    height: 200,
+    backgroundColor: "teal" 
+},
+esquerda:{
+    width: 200,
+    height: 200,
+    backgroundColor: "orange" 
+},
+ texto: {
+    color: "#eee",
+     fontSize: 20,
+  },
+})
+
+export default function AtvMudaConteudo(){
+    const [pagina, setPagina] = useState(0)
+
+        function primeira(){
+            setPagina(1)
+        }
+          function segunda(){
+            setPagina(-1)
+        }
+
+    return(
+        <View>
+            <View style={{flexDirection: "row"}}>
+                <Pressable onPress={() => primeira()} style={Estilinhos.botoes}>
+                    <Text> botão 1 </Text>
+                </Pressable>
+                <Pressable onPress={() => segunda()} style={Estilinhos.botoes}>
+                    <Text> botão 2 </Text>
+                </Pressable>
+            </View>
+           
+
+
+
+            {pagina === 1 && (
+                <Text style={Estilinhos.texto}>Página em construção</Text>
+            )}
+
+            {pagina === -1 && (
+                <View style={{ flexDirection: "row" }}>
+                    <View style={Estilinhos.direita} />
+                      <View style={Estilinhos.meio} />
+                      <View style={Estilinhos.esquerda} />
+                </View>
+            )}
+        </View>
+
+    )
+}
