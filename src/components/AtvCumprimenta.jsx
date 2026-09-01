29a50910f43f73de7ab881cece7d03e2916543a4
JonhPerter07 <joaopedro.rodrigues@nave.org.br>
2026-09-01 10:39:39 -0300

    Update AtvCumprimenta.jsx
    feito.recuperação

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,73 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+
+
+import React, { useState } from "react";
+import {View,Text,TextInput,Pressable,StyleSheet,} from "react-native";
+
+export default function AtvCumprimenta() {
+  const [nome, setNome] = useState("");
+  const [nomeSalvo, setNomeSalvo] = useState("");
+
+  return (
+    <View style={styles.caixa}>
+      <Text style={styles.pergunta}> Olá, qual seu nome?</Text>
+
+      <View style={styles.linha}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome aqui"
+          value={nome}
+          onChangeText={setNome}
+        />
+        <Pressable
+          style={styles.botao}
+          onPress={() => setNomeSalvo(nome)}
+        >
+          <Text style={styles.textoBotao}>Salvar</Text>
+        </Pressable>
+      </View>
+
+      {nomeSalvo == "" && (<Text style={styles.mensagem}> É um prazer lhe conhecer, 
+      {nomeSalvo}
+        </Text>
+      )}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,},
+
+  pergunta: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  linha: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#999",
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  botao: {
+    backgroundColor: "#4CAF50",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+  },
+  textoBotao: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  mensagem: {
+    marginTop: 20,
+    fontSize: 16,
+  },
+});
